e9230c32e3d2c337cbb1e6e2bb167439ac528dd1
hec <hdepaz90@gmail.com>
2026-04-18 04:05:40 -0400

    Remove custom-set-faces from custom.el
    Removed custom-set-faces section from custom.el.

--- custom.el
+++ custom.el
@@ -10,9 +10,3 @@
      "8eabe9aa600059ad7c1c110c35a98f8601b17d3f44efc526250671ad5b5ac1cf" default))
  '(package-vc-selected-packages
    '((claude-code-ide :url "https://github.com/manzaltu/claude-code-ide.el"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
